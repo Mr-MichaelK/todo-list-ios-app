f20bca59f598c88d744f1c902050f61a6776100d
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-06-07 20:56:58 +0300

    Set up file structure

---
+++ ToDoListApp/Coordinators/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  ToDoListApp
+//
+//  Created by Michael Kolanjian on 07/06/2025.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    var navigationController: UINavigationController { get set }
+    func start()
+}
